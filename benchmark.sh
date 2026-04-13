3429a3e73cb797eb081f20aba379566a6c0f2ddf
jyothishgopa <jyothishgopa@wisc.edu>
2026-04-13 15:30:30 -0500

    fixed benchmar.sh for res 0

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 > output.txt
-./build/setup.sh
+./setup.sh
 
-for ((res=0; res<=64; res*=2)); do
+for i in {1..5}; do
+    echo "Standard run $i at res 0"
+    echo "Standard run $i at res 0" >> output.txt
+    ./build/bin/jz_FEA_3243_check --res 0 --nthreads 16 >> output.txt 2>&1
+done
+
+# Sub-loop 2: CUDSS Check (5 runs)
+for i in {1..5}; do
+    echo "CUDSS run $i at res 0"
+    echo "CUDSS run $i at res 0s" >> output.txt
+    ./build/bin/jz_FEA_3243_check_cudss --res 0 --nthreads 16 >> output.txt 2>&1
+done
+
+for ((res=2; res<=64; res*=2)); do
     echo "--- Starting Resolution: $res ---"
     echo "--- Starting Resolution: $res ---" >> output.txt
 
